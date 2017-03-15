9f1ed4835f688087c39479e40668948116cc53a3
Aram Koukia <aramkoukia@gmail.com>
2017-03-15 16:54:33 -0700

    added trail list component

--- mycityweb/src/app/app-routing.module.ts
+++ mycityweb/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CityComponent } from './components/city/city.component';
+import { TrailListComponent } from './components/trail/trail-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
-  { path: 'area/:id', component: CityComponent }
+  { path: 'area/:id', component: CityComponent },
+  { path: 'area/:id/trails', component: TrailListComponent }
 ];
 
 @NgModule({
---
+++ mycityweb/src/app/components/trail/trail-list.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CitiesService } from '../../services/cities.service';
+import { City } from '../../models/city';
+
+@Component({
+    selector: 'trail-list',
+    templateUrl: './trail-list.component.html',
+})
+export class TrailListComponent {
+    public cities: City[] = [];
+
+    constructor(private citiesService: CitiesService, private router: Router) { }  
+    
+    public gotoDetail(trail) {
+        // todo: should use title to be displayed in the browser
+        let link = ['/trail', trail.id];
+        this.router.navigate(link);
+    }
+
+    ngOnInit(): void {
+        this.citiesService.getCities()
+            .then(cities => this.cities = cities);
+    }
+
+}
