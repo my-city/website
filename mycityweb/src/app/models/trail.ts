7579e296e12b0eb6e67e9a1f765062b084541e8f
Aram Koukia <aramkoukia@gmail.com>
2017-03-16 14:29:24 -0700

    some fixes to the trail list page

--- mycityweb/src/app/models/trail.ts
+++ mycityweb/src/app/models/trail.ts
@@ -1,8 +1,37 @@
 ﻿export class Trail {
     id: string;
-    country: string;
-    state: string;
-    title: string;
-    description: string;
-    image: string;
+    category: string; //ie: vancouver
+    name: string;
+    approved: boolean;
+    description: string; // html
+    rating: number;
+    voteCount: number;
+    region: string;    //ie: Tri-city
+    difficulty: string;  // hard, medium, easy
+    time: number;   // hours/minutes
+    distance: number;  // ie: from Vanouver
+    camping: boolean;
+    dogFriendly: boolean;
+    publicTransit: boolean;
+    mapImages: {
+        path: string,
+        title: string
+    }[];
+    images: {
+        path: string,
+        title: string
+    }[];
+    geoLocation: {
+        address: string;
+        coordinates: number[];
+    };
+    comments: {
+        user: string,
+        email: string,
+        comment: string,
+        images: {
+            path: string,
+            title: string
+        }[];
+    }[];
 }
